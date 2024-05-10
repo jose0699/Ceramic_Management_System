da200449f6ff01017d5938e6032b645deff996d6
jose0699 <135712054+jose0699@users.noreply.github.com>
2024-05-09 22:11:47 -0400

    Inicialización
    La carpeta de BD contendrá todo lo respecto a la Base de datos. Esta dividido en 3 archivos sql, su nombre contiene el tópico del CRUD que tomara.

---
+++ P_BD_2024/BD/Insert.sql
@@ -0,0 +1,29 @@
+/*
+Este archivo va a contener todo lo respecto a las inserciones.
+	Orden del documento:
+		1-Tablas fuertes
+		2-Intermedia
+		3-Intercepción
+Nota: Se ordenara segun el proceso en especifico que participa la tabla.
+*/
+
+/*----------Tablas Fuertes----------*/
+--Proceso de Catalogo
+
+--Proceso Venta
+
+--Proceso de Control Empleado
+
+/*----------Tablas Intermedia----------*/
+--Proceso de Catalogo
+
+--Proceso Venta
+
+--Proceso de Control Empleado
+
+/*----------Tablas Intercepción----------*/
+--Proceso de Catalogo
+
+--Proceso Venta
+
+--Proceso de Control Empleado
---
+++ P_BD_2024/BD/Triggers_Procedure_Function_Index.sql
@@ -0,0 +1,10 @@
+/*
+Este archivo tiene la función de contener toda la información relacionada con las funciones, procedimientos, 
+triggers e índices del sistema.
+	El orden a seguir es:
+		1-Procedure
+		2-Function
+		3-Trigger
+		4-Index
+*/
+

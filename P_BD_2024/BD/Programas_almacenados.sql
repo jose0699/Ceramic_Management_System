d48fbfcd0a945e5409372da72188df08023047bb
Franklin Quintana <120291782+Franku03@users.noreply.github.com>
2024-06-16 03:47:48 -0400

    +3 REPORTES agregados, programa de almacenado 6

--- P_BD_2024/BD/Programas_almacenados.sql
+++ P_BD_2024/BD/Programas_almacenados.sql
@@ -12,11 +12,11 @@ CREATE OR REPLACE PROCEDURE rotacion_turnos_horneros() AS $$
   DECLARE
       h record;
   BEGIN
-      FOR h IN SELECT num_expediente, mesano, turno   
+      FOR h IN SELECT num_expediente, mes_inicioano, turno   
                   FROM hist_turno 
-                  WHERE mesano = date_trunc('month', now()) - interval '1 month'
+                  WHERE mes_inicioano = date_trunc('month', now()) - interval '1 month'
       LOOP
-        INSERT INTO hist_turno values (h.num_expediente, h.mesano + interval '1 month', CASE
+        INSERT INTO hist_turno values (h.num_expediente, h.mes_inicioano + interval '1 month', CASE
 				                                                    WHEN h.turno = 1 THEN 2
 				                                                    WHEN h.turno = 2 THEN 3
 				                                                    WHEN h.turno = 3 THEN 1
@@ -29,50 +29,86 @@ $$ LANGUAGE plpgsql;
 --------------------------------------------------------------------------------------------------------
 --                                           Function                                                --
 --------------------------------------------------------------------------------------------------------
-/*
-CREATE OR REPLACE FUNCTION porcentaje_inasistencia() AS $$
-DECLARE 
 
-BEGIN
+--NUMERO 6
+CREATE OR REPLACE FUNCTION obtener_dias_no_laborales() RETURNS numeric(2) AS $$
+  DECLARE
+    dias_no_laborales numeric(2);
+    dias_feriados numeric(1);
+  BEGIN
+      -- Esta SELECT genera una serie con la cual se obtienen los dias del mes_inicio y se le extraen a através del campo DOW(days of weekend)
+      -- los domingos, que son días que la fábrica no está operativa
+      SELECT count(*) INTO dias_no_laborales
+      FROM generate_series(date_trunc('month', now()), 
+                date_trunc('month', now()) + '1 month'::interval - '1 day'::interval,
+                          '1 day'::interval) dias_laborales(d)
+      WHERE extract(DOW FROM dias_laborales.d) IN (0);
 
-END;
-$$ LANGUAGE plpgsql
+      -- Se verifica en caso de ser un mes con feriado
+      SELECT CASE 
+        WHEN to_char(current_date,'MM') = '12' THEN 3
+        WHEN to_char(current_date, 'MM') = '01' THEN 1
+        WHEN to_char(current_date, 'MM') = '05' THEN 1
+        ELSE 0
+      END INTO dias_feriados;
 
-CREATE OR REPLACE FUNCTION porcentaje_inasistencia_reunion(fecha DATE, nombre varchar(61))
-RETURNS NUMERIC AS $$
-DECLARE
-  expediente NUMERIC(4);
-  inasistencia NUMERIC(4);
-  total_clases NUMERIC(4);
-  porcentaje NUMERIC(5,2);
-BEGIN
-  -- Obtener el número de expediente del supervisor
-  SELECT e.num_expediente
-  INTO expediente
-  FROM empleado e
-  WHERE trabaja = $1
-    AND e.primer_nombre || ' ' || e.primer_apellido = nombre;
+      dias_no_laborales := dias_no_laborales + dias_feriados;
 
-  -- Obtener la cantidad de inasistencias del supervisor
-  SELECT COUNT(*)
-  INTO inasistencia
-  FROM inasistencia i
-  WHERE i.num_expediente_supervisor = expediente
-    AND i.fecha_hora BETWEEN $1 AND $1 + INTERVAL '1 month';
-
-  -- Obtener el total de clases del supervisor
-  SELECT COUNT(*) * 4
-  INTO total_clases
-  FROM empleado emp
-  WHERE emp.supervisor = expediente OR  emp.num_expediente = expediente;
-
-  -- Calcular el porcentaje de inasistencia
-  porcentaje := (inasistencia * 100.0) / total_clases;
-
-  RETURN porcentaje;
-END;
+      RETURN dias_no_laborales;
+  END;
 $$ LANGUAGE plpgsql;
-*/
+
+
+CREATE OR REPLACE FUNCTION obtener_dias_laborales(v_cargo varchar(2), v_departamento numeric(2)) RETURNS numeric(2) AS $$
+  DECLARE
+    dias_laborales numeric;
+  BEGIN
+    SELECT date_part('days', 
+            date_trunc('month', now()) 
+            + '1 month'::interval
+            - '1 day'::interval
+        )::numeric INTO dias_laborales;
+
+
+      IF(v_cargo = 'og' AND v_departamento = 15) THEN
+          RETURN dias_laborales;
+      END IF; 
+
+      dias_laborales := dias_laborales - obtener_dias_no_laborales();
+
+    RETURN dias_laborales;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION porcentaje_inasistencia_empleado(v_id_empleado IN empleado.num_expediente%TYPE, mes_inicio date) RETURNS varchar(4) AS $$
+  DECLARE
+    dias_laborales numeric(2);
+    inasistencias numeric(2);
+    porcentaje_inac numeric(5,2);
+    --mes_inicio date;
+    mes_fin date;
+    v_cargo varchar(2);
+    v_departamento numeric(2);
+  BEGIN
+      --mes_inicio := date_trunc('month', now());
+      mes_fin := mes_inicio + interval '1 month' - interval '1 day';
+
+   		SELECT e.cargo, e.trabaja FROM EMPLEADO e WHERE e.num_expediente = v_id_empleado INTO v_cargo, v_departamento;
+
+      dias_laborales := obtener_dias_laborales(v_cargo, v_departamento);  
+      --raise notice 'dl: %', dias_laborales;
+      SELECT COUNT(d.num_exp) INTO inasistencias FROM det_exp d WHERE d.num_exp = v_id_empleado AND fecha BETWEEN mes_inicio AND mes_fin;
+      --raise notice 'inac: %', inasistencias;
+      porcentaje_inac := 100-(inasistencias/dias_laborales) * 100;
+      --raise notice 'Value: %', porcentaje_inac;
+      RETURN CONCAT('%', to_char(porcentaje_inac,'990'));
+  END;
+$$ LANGUAGE plpgsql;
+--Hasta aquí es el número 6
+
+
+
+
 
 
 
@@ -80,7 +116,7 @@ $$ LANGUAGE plpgsql;
 --                           Funciones y Procedimientos Menores                                       --
 --------------------------------------------------------------------------------------------------------
 
-CREATE OR REPLACE FUNCTION edad (fec_nac date) RETURNS date AS $$
+CREATE OR REPLACE FUNCTION edad (fec_nac date) RETURNS integer AS $$
 	BEGIN
 		RETURN (round(((current_date- fec_nac)/365),0));
 	END; $$ LANGUAGE plpgsql;

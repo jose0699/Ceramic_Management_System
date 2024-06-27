c59c0bf442971df961d6607ae74f9af76bbd9ae7
jose0699 <joseluislopezasencio704@gmail.com>
2024-06-27 18:53:52 -0400

    Pedido + Posible Fecha de entrega

--- P_BD_2024/BD/Tercera Entrega/Programa_Entrega_Tres.sql
+++ P_BD_2024/BD/Tercera Entrega/Programa_Entrega_Tres.sql
@@ -56,6 +56,70 @@ SELECT CAST((
 	WHERE pe.uid_pedido = 1 AND dt.uid_juego IS NULL AND fh.fecha_fin IS NULL) 
 AS DECIMAL(8,2));
 
+---------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------
+call ASIGNAR_FECHA_ENTREGA(1);
+SELECT * FROM PEDIDO pe WHERE pe.fecha_emision = emision;
+select * from pedido ORDER BY FECHA_EMISION;
+select * from contrato
 
+CREATE OR REPLACE FUNCTION CONFLICTO_FECHA (emision DATE) RETURNS DATE AS $$
+DECLARE
+	aux numeric(1);
+BEGIN
+	--Tabla de todos los contratos activos
+		CREATE TEMPORARY TABLE CLIENTE_CONTRATO ( pk_cliente numeric(3), pk_contrato numeric(5));
+		INSERT INTO CLIENTE_CONTRATO (pk_cliente, pk_contrato)
+			SELECT con.uid_cliente, con.num_contrato FROM CONTRATO con WHERE con.fecha_hora_fin IS NULL;
+			
+	--Tabla de todos los pedidos en estado emitido
+		CREATE TEMPORARY TABLE CLIENTE_MODIFICAR (pk_cliente numeric(3), pk_pedido NUMERIC(6), old_entrega DATE);
+		INSERT INTO CLIENTE_MODIFICAR (pk_pedido, old_entrega) 
+			SELECT po.uid_cliente, po.uid_pedido, po.fecha_entrega FROM PEDIDO po		
+			WHERE po.estado = 'E' AND po.fecha_entrega >= emision;
+		
+	--Se busca si el cliente tiene un contrato en la emision
+		SELECT COUNT(*) INTO aux FROM CLIENTE_CONTRATO cc 
+		WHERE cc.pk_cliente IN (SELECT cm.pk_cliente FROM CLIENTE_MODIFICAR cm WHERE cm.old_entrega = emision)
+			
+	IF aux = 0 THEN
+		SELECT clmo.old_entrega INTO emision FROM CLIENTE_MODIFICAR clmo ORDER BY clmo.old_entrega ASC LIMIT 1;
+		emision := emision + Interval '1 DAY';		
+	ELSE 
+		SELECT mo.old_entrega FROM CLIENTE_MODIFICAR mo 
+		WHERE mo.pk_cliente NOT IN (SELECT co.uid_cliente FROM CLIENTE_CONTRATO co) ORDER BY old_entrega DESC limit 1;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION ASIGNAR_FECHA_ENTREGA(emision DATE) RETURNS DATE AS $$
+DECLARE
+	estados varchar(1);
+	aux numeric(1);
+BEGIN
+	--Se busca la fecha de emision del pedido emitido.
+		SELECT p.fecha_emision INTO emision, p.estado into estados FROM pedido p WHERE p.uid_pedido = pedido;
+		
+	--Posible fecha de entrega
+		emision := emision + interval '2 months';
+					
+	--Se busca si existe alguien con la misma fecha, si no hay nadie es la fecha de entrego sino, se buscara posibles fecha de entrega.
+		SELECT COUNT(*) INTO aux FROM PEDIDO pe WHERE pe.fecha_entrega = emision AND pe.estado = 'E';
+	
+	IF aux = 0 THEN
+			RAISE NOTICE 'La posible fecha de entrega es: %', emision;	
+	ELSE
+		IF estados = 'C' THEN
+			RETURN emision;
+			RAISE NOTICE 'La posible fecha de entrega 1 es: %', emision;	
+		ELSE 
+			emision:= CONFLICTO_FECHA(emision);
+			RAISE NOTICE 'La posible fecha de entrega 2 es: %', emision;	
+		END IF;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
 
 					
--- P_BD_2024/BD/Tercera Entrega/Reportes_Entrega_Tres.sql
+++ P_BD_2024/BD/Tercera Entrega/Reportes_Entrega_Tres.sql
@@ -1,6 +1,10 @@
 /*Reportes*/
 
---Pedido Completo
+--------------------------------------------------------------------------------------------------------------------
+-----                                             Pedido completo                                           --------
+--------------------------------------------------------------------------------------------------------------------
+
+
 CREATE OR REPLACE FUNCTION DATOS_BASICO_PEDIDO (num_pedido numeric(6))
 	RETURNS TABLE (
 		uid_pedido numeric(6),
@@ -47,47 +51,128 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+--Subreporte Piezas del pedido
+	SELECT d.uid_detalle, p.uid_pieza, c.nombre AS nombre_col, m.molde, d.cantidad,	p.precio * d.cantidad AS p_inst, f.precio * d.cantidad AS p_fami
+	FROM detalle_pedido_pieza d
+		JOIN pedido x ON x.uid_pedido = d.uid_pedido
+		JOIN pieza p ON p.uid_pieza = d.uid_pieza
+		LEFT JOIN familiar_historico_precio f ON p.uid_pieza = f.uid_pieza AND f.fecha_inicio::date = obtener_fecha_historico(p.uid_pieza,x.fecha_entrega)
+		JOIN nombres_moldes m ON m.uid_molde = p.uid_molde
+		JOIN coleccion c ON c.uid_coleccion = p.uid_coleccion
+	WHERE d.uid_pedido = $P{id_Pedido} ORDER BY uid_detalle DESC;
+	
+--Subreporte Vajillas del pedido
+	SELECT DISTINCT d.uid_detalle, v.uid_juego,	c.nombre AS nombre_col,	v.nombre AS nombre_vaj,	d.cantidad,	calcular_precio_vajilla(v.uid_juego, x.uid_coleccion, p.fecha_entrega) precio
+	FROM detalle_pedido_pieza d
+		JOIN pedido p ON p.uid_pedido = d.uid_pedido
+		JOIN vajilla v ON v.uid_juego = d.uid_juego
+		JOIN detalle_pieza_vajilla x ON v.uid_juego = x.uid_juego
+		JOIN coleccion c ON c.uid_coleccion = x.uid_coleccion
+	WHERE d.uid_pedido = $P{id_Pedido} 	ORDER BY uid_detalle DESC;
 
+--------------------------------------------------------------------------------------------------------------------
+-----                                                Factura completa                                       --------
+--------------------------------------------------------------------------------------------------------------------
+
+select * from DATOS_BASICO_FACTURA(1)
+
+CREATE OR REPLACE FUNCTION DATOS_BASICO_FACTURA (num_factura numeric(6))
+	RETURNS TABLE (
+		uid_pedido numeric(6),
+		fecha_emision text,
+		nombre_cliente  varchar(50),
+		telefono_cliente varchar(15),
+		email_cliente varchar(256),
+		monto_sin_descuento numeric (8,2),
+		Descuento numeric (8,2),
+		monto_total numeric(8,2)
+	)
+AS $$
+DECLARE
+	monto_sin_descuento numeric (8,2);
+	Descuento numeric (8,2);
+    monto numeric(8,2);
+BEGIN
+	--Descuento ofrecido
+	SELECT COALESCE (c.porcentaje_descuento, 0) INTO Descuento
+	FROM CLIENTE cl
+		 INNER JOIN CONTRATO c ON cl.uid_cliente = c.uid_cliente 
+ 		 INNER JOIN PEDIDO p ON cl.uid_cliente = p.uid_cliente 
+		 INNER JOIN FACTURA fac ON fac.uid_pedido = p.uid_pedido
+	WHERE fac.numero_factura = num_factura AND c.fecha_hora_fin IS NULL;
+	
+	--Monto sin el descuento
+	SELECT COALESCE(SUM(dt.cantidad * fh.precio), 0) +
+		   COALESCE((
+			   SELECT COALESCE(SUM(d.cantidad * (fam.precio * dv.cantidad)) * 0.85, 0) 
+			   FROM FACTURA f
+			   		INNER JOIN pedido p ON f.uid_pedido = p.uid_pedido
+					INNER JOIN DETALLE_PEDIDO_PIEZA d ON p.uid_pedido = d.uid_pedido
+					INNER JOIN DETALLE_PIEZA_VAJILLA dv ON d.uid_juego = dv.uid_juego
+					INNER JOIN PIEZA pz ON pz.uid_pieza = dv.uid_pieza
+					INNER JOIN familiar_historico_precio fam ON pz.uid_pieza = fam.uid_pieza
+			   WHERE f.numero_factura = num_factura AND 
+			   		 d.uid_juego IS NOT NULL AND 
+			   		 fam.fecha_fin IS NULL), 0) INTO  monto_sin_descuento		
+	FROM FACTURA fa
+		INNER JOIN PEDIDO pe ON fa.uid_pedido = pe.uid_pedido
+		INNER JOIN DETALLE_PEDIDO_PIEZA dt ON pe.uid_pedido = dt.uid_pedido
+		INNER JOIN PIEZA pi ON pi.uid_pieza = dt.uid_pieza
+		INNER JOIN familiar_historico_precio fh ON pi.uid_pieza = fh.uid_pieza
+	WHERE fa.numero_factura = num_factura  AND 	
+		  dt.uid_juego IS NULL AND 
+		  fh.fecha_fin IS NULL;	
+	
+	IF Descuento <> 0 THEN
+		monto := (monto_sin_descuento * (1 -(Descuento/100))):: Numeric(8,2);
+	ELSE 
+		monto := (monto_sin_descuento)::Numeric(8,2);
+	END IF;
+	
+	RETURN QUERY
+	SELECT 
+		   p.uid_pedido,
+		   to_char(fact.fecha_emision, 'DD "de" TMMonth "de" YYYY'),
+		   c.nombre,
+		   c.telefono,
+		   c.email,
+		   monto_sin_descuento,
+		   Descuento,
+		   monto
+	FROM CLIENTE c
+		INNER JOIN PEDIDO p ON c.uid_cliente = p.uid_cliente
+		INNER JOIN FACTURA fact ON fact.uid_pedido = p.uid_pedido
+	WHERE fact.numero_factura = num_factura; 
+END;
+$$ LANGUAGE plpgsql;
 
 --Subreporte Piezas del pedido
-SELECT 
-	d.uid_detalle,
-	p.uid_pieza,
-	c.nombre AS nombre_col,
-	m.molde,
-	d.cantidad,
-	p.precio * d.cantidad AS p_inst,
-	f.precio * d.cantidad AS p_fami
-FROM detalle_pedido_pieza d
-JOIN pedido x ON x.uid_pedido = d.uid_pedido
-JOIN pieza p ON p.uid_pieza = d.uid_pieza
-LEFT JOIN familiar_historico_precio f ON p.uid_pieza = f.uid_pieza AND f.fecha_inicio::date = obtener_fecha_historico(p.uid_pieza,x.fecha_entrega)
-JOIN nombres_moldes m ON m.uid_molde = p.uid_molde
-JOIN coleccion c ON c.uid_coleccion = p.uid_coleccion
-WHERE d.uid_pedido = $P{id_Pedido} 
-ORDER BY uid_detalle DESC;
+	SELECT 
+		d.uid_detalle, 	p.uid_pieza, c.nombre AS nombre_col, m.molde, d.cantidad,
+		COALESCE (p.precio * d.cantidad, 0) AS p_inst, COALESCE(f.precio * d.cantidad, 0) AS p_fami
+	FROM detalle_pedido_pieza d
+		JOIN pedido x ON x.uid_pedido = d.uid_pedido
+		JOIN pieza p ON p.uid_pieza = d.uid_pieza
+		LEFT JOIN familiar_historico_precio f ON p.uid_pieza = f.uid_pieza AND f.fecha_inicio::date = obtener_fecha_historico(p.uid_pieza,x.fecha_entrega)
+		JOIN nombres_moldes m ON m.uid_molde = p.uid_molde
+		JOIN coleccion c ON c.uid_coleccion = p.uid_coleccion
+	WHERE d.uid_pedido in (SELECT fr.uid_pedido FROM FACTURA fr WHERE fr.numero_factura = $P{id_factura}) ORDER BY uid_detalle DESC;
 
 --Subreporte Vajillas del pedido
-SELECT DISTINCT
-	d.uid_detalle,
-	v.uid_juego,
-	c.nombre AS nombre_col,
-	v.nombre AS nombre_vaj,
-	d.cantidad,
-	calcular_precio_vajilla(v.uid_juego, x.uid_coleccion, p.fecha_entrega) precio
-FROM detalle_pedido_pieza d
-JOIN pedido p ON p.uid_pedido = d.uid_pedido
-JOIN vajilla v ON v.uid_juego = d.uid_juego
-JOIN detalle_pieza_vajilla x ON v.uid_juego = x.uid_juego
-JOIN coleccion c ON c.uid_coleccion = x.uid_coleccion
-WHERE d.uid_pedido = $P{id_Pedido} 
-ORDER BY uid_detalle DESC;
+	SELECT DISTINCT
+		d.uid_detalle, v.uid_juego, c.nombre AS nombre_col, v.nombre AS nombre_vaj, d.cantidad, 
+		calcular_precio_vajilla(v.uid_juego, x.uid_coleccion, p.fecha_entrega) precio
+	FROM detalle_pedido_pieza d
+		JOIN pedido p ON p.uid_pedido = d.uid_pedido
+		JOIN vajilla v ON v.uid_juego = d.uid_juego
+		JOIN detalle_pieza_vajilla x ON v.uid_juego = x.uid_juego
+		JOIN coleccion c ON c.uid_coleccion = x.uid_coleccion			
+	WHERE d.uid_pedido in (SELECT fr.uid_pedido FROM FACTURA fr WHERE fr.numero_factura = $P{id_factura}) ORDER BY uid_detalle DESC;
 
 
---Factura completa 
-
-
---Informe mensual de Venta
+--------------------------------------------------------------------------------------------------------------------
+-----                                    Informe mensual de Venta                                           --------
+--------------------------------------------------------------------------------------------------------------------
 SELECT * FROM FACTURA
 
 CALL INFORME_MENSUAL_VENTA('2024-03-10');

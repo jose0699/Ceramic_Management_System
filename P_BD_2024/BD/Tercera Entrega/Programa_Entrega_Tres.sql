a3ddb3abb86686f9b342f410343428bea92b2f41
jose0699 <joseluislopezasencio704@gmail.com>
2024-07-05 18:36:24 -0400

    Insertar pedido

--- P_BD_2024/BD/Tercera Entrega/Programa_Entrega_Tres.sql
+++ P_BD_2024/BD/Tercera Entrega/Programa_Entrega_Tres.sql
@@ -1,10 +1,11 @@
-CREATE OR REPLACE INSERTAR_PEDIDO() AS $$
-DECLARE
-	
+CREATE OR REPLACE PROCEDURE INSERTAR_PEDIDO(cliente numeric(6), tipo varchar(1)) AS $$
 BEGIN
-
+	IF tipo IN ('F', 'I') THEN
+		INSERT INTO PEIDO VALUES (cliente, nextval('pedido_uid_seq'), current_date, null, (SELECT * FROM ASIGNAR_FECHA_ENTREGA), 'E', tipo);
+	END IF;
 END;
 $$ LANGUAGE plpgsql;
+
 
 ---------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------
@@ -22,44 +23,46 @@ DECLARE
 	tipo varchar(1);
 	tipo_producto varchar(1);
 BEGIN
-	--Se busca la cantidad de detalle existente del pedido.
-	select count(*) into num_detalle FROM detalle_pedido_pieza dpp WHERE dpp.uid_pedido = pedido; 
-	
-	--Incrementa el número del detalle.
-	num_detalle := num_detalle + 1;
-		
-	--Se busca la pk del cliente y el tipo de pedido (F o I).
-	SELECT p.uid_cliente, p.tipo_pedido, p.estado INTO cliente, tipo, estado_pedido FROM PEDIDO p WHERE p.uid_pedido = pedido;
-	
-	--Si el estado = Emitido, puede insertar
-	IF estado_pedido = 'E' THEN
-		--Si tipo = 1 entonces estamos manejando una Vajilla.
-		IF tipo_pedido = 1 THEN
-			--Se busca la linea de la vajilla.
-			SELECT co.linea into tipo_producto FROM COLECCION co
-				INNER JOIN DETALLE_PIEZA_VAJILLA dpv ON co.uid_coleccion = dpv.uid_coleccion 
-			WHERE uid_juego = producto Limit 1;
+	IF (SELECT pedi.tipo_pedido FROM PEDIDO pedi WHERE pedi.uid_pedido = pedido) = 'E' THEN
+		--Se busca la cantidad de detalle existente del pedido.
+		select count(*) into num_detalle FROM detalle_pedido_pieza dpp WHERE dpp.uid_pedido = pedido; 
 
-			--Se verifica si coincide con la linea del pedido
-			IF tipo = tipo_producto THEN
-				insert into DETALLE_PEDIDO_PIEZA values( cliente, pedido, num_detalle, cantidad , producto);
-			ELSE
-				RAISE EXCEPTION 'Error: Productos distintos';
+		--Incrementa el número del detalle.
+		num_detalle := num_detalle + 1;
+
+		--Se busca la pk del cliente y el tipo de pedido (F o I).
+		SELECT p.uid_cliente, p.tipo_pedido, p.estado INTO cliente, tipo, estado_pedido FROM PEDIDO p WHERE p.uid_pedido = pedido;
+
+		--Si el estado = Emitido, puede insertar
+		IF estado_pedido = 'E' THEN
+			--Si tipo = 1 entonces estamos manejando una Vajilla.
+			IF tipo_pedido = 1 THEN
+				--Se busca la linea de la vajilla.
+				SELECT co.linea into tipo_producto FROM COLECCION co
+					INNER JOIN DETALLE_PIEZA_VAJILLA dpv ON co.uid_coleccion = dpv.uid_coleccion 
+				WHERE uid_juego = producto Limit 1;
+
+				--Se verifica si coincide con la linea del pedido
+				IF tipo = tipo_producto THEN
+					insert into DETALLE_PEDIDO_PIEZA values( cliente, pedido, num_detalle, cantidad , producto);
+				ELSE
+					RAISE EXCEPTION 'Error: Productos distintos';
+				END IF;
 			END IF;
-		END IF;
-		
-		--Si tipo = 2 entonces estamos manejando una Pieza
-		IF tipo_pedido = 2 THEN
-			--Se busca la pk y linea de la coleccion de la pieza
-			SELECT pi.uid_coleccion, col.linea  into coleccion, tipo_producto FROM PIEZA pi
-				INNER JOIN COLECCION col ON col.uid_coleccion = pi.uid_coleccion
-			WHERE pi.uid_pieza = producto;
 
-			--Se verifica si coincide con la linea del pedido
-			IF tipo = tipo_producto THEN
-				insert into DETALLE_PEDIDO_PIEZA values( cliente, pedido, num_detalle, cantidad, null, coleccion, producto);
-			ELSE
-				RAISE EXCEPTION 'Error: Productos distintos';
+			--Si tipo = 2 entonces estamos manejando una Pieza
+			IF tipo_pedido = 2 THEN
+				--Se busca la pk y linea de la coleccion de la pieza
+				SELECT pi.uid_coleccion, col.linea  into coleccion, tipo_producto FROM PIEZA pi
+					INNER JOIN COLECCION col ON col.uid_coleccion = pi.uid_coleccion
+				WHERE pi.uid_pieza = producto;
+
+				--Se verifica si coincide con la linea del pedido
+				IF tipo = tipo_producto THEN
+					insert into DETALLE_PEDIDO_PIEZA values( cliente, pedido, num_detalle, cantidad, null, coleccion, producto);
+				ELSE
+					RAISE EXCEPTION 'Error: Productos distintos';
+				END IF;
 			END IF;
 		END IF;
 	END IF;
@@ -141,11 +144,14 @@ END;
 $$ LANGUAGE plpgsql;
 ---------------------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------------------
+BEGIN;	insert into factura values( 1,1,nextval('factura_uid_seq') ,'2024-01-18 10:50:00' , 943.5 );	COMMIT;--1
+
+
 CREATE OR REPLACE FUNCTION MODIFICAR_PEDIDO() RETURNS TRIGGER AS $$
 DECLARE
 BEGIN
 	IF new.estado = 'A' THEN 
-			
+		INSERT INTO factura values(old.uid_cliente, old.uid_pedido, nextval('factura_uid_seq'), current_date, );
 	END IF;
 END;
 $$ LANGUAGE plpgsql;
